6d6deb6139d2da431b10bf1765ad10311b42cdd7
pointyteeth <xxxmeenaxxx@gmail.com>
2014-03-02 21:32:55 -0800

    first commit

---
+++ i/Assets/Scripts/offsetMaterial.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start() {
+	renderer.material.mainTextureOffset = Vector2 (Offset(), Offset());
+}
+
+function Update() {
+
+}
+
+function Offset() {
+	return Random.Range(0, 4)*0.25;
+}
